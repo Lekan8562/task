4ed0981928e19795a63eaddbdc1d4da77e214fb5
Jegede Olajide Olamilekan <138625724+Lekan8562@users.noreply.github.com>
2024-07-01 22:10:08 +0100

    code commit

---
+++ api/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+import requests
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import GreetingSerializer
+
+# Create your views here.
+@api_view(['GET'])
+def hello(request):
+    visitor_name = request.GET.get('visitor_name', 'Visitor')
+    client_ip = request.META.get('REMOTE_ADDR')
+    location = 'Akure'
+    temperature = 27
+    greeting = f"Hello, {visitor_name}. the temperature is  {temperature} degrees celcius in {location}"
+
+    data = {
+        'client_ip':client_ip,
+        'location':location,
+        'greeting':greeting
+    }
+    serializer = GreetingSerializer(data)
+    return Response(serializer.data)
